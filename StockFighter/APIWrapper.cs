9c9e4a8d75f9177bb787dbd5c34b530c20819631
bretgourdie <adjectivenounisadjective@gmail.com>
2016-05-23 13:21:13 -0400

    Added documentation to APIWrapper methods.

--- StockFighter/APIWrapper.cs
+++ StockFighter/APIWrapper.cs
@@ -10,13 +10,30 @@ using System.Net.Http;
 
 namespace StockFighter
 {
+    /// <summary>
+    /// Static class to wrap Stockfighter API functions
+    /// </summary>
     public class APIWrapper
     {
+        /// <summary>
+        /// API URL for interfacing.
+        /// </summary>
         private const string URL = @"https://api.stockfighter.io/ob/api/";
+        /// <summary>
+        /// Default API Key for authorization.
+        /// </summary>
         private const string apiKey = @"b5b8b7f29d5aa969da22279262c1e68ff82515c4";
+        /// <summary>
+        /// Specification of JSON media type.
+        /// </summary>
         private const string jsonMedia = @"application/json";
 
         #region API Calls
+
+        /// <summary>
+        /// Checks if the server is online.
+        /// </summary>
+        /// <returns>Returns true if the server is up; else, false.</returns>
         public static bool Heartbeat()
         {
             var client = getClient();
@@ -38,6 +55,11 @@ namespace StockFighter
             }
         }
 
+        /// <summary>
+        /// Checks if a venue is available.
+        /// </summary>
+        /// <param name="venue">The venue to check if available.</param>
+        /// <returns>Returns true if the venue is available; else, false.</returns>
         public static bool CheckVenue(string venue)
         {
             var client = getClient();
@@ -62,6 +84,10 @@ namespace StockFighter
         #endregion
         #region Privates
 
+        /// <summary>
+        /// Assembles a client with the default API key.
+        /// </summary>
+        /// <returns>Returns an instantiated, authorized RestClient.</returns>
         private static RestClient getClient()
         {
             var client = new RestClient(URL);
@@ -69,6 +95,17 @@ namespace StockFighter
             return client;
         }
 
+        /// <summary>
+        /// Translation matrix for a command enum to the URL.
+        /// </summary>
+        /// <remarks>
+        /// Any REST parameters will appear in the returned string as format hooks.
+        /// </remarks>
+        /// <param name="command">The command to retrieve the REST string for.</param>
+        /// <returns>
+        /// Returns a command string with the parameters 
+        /// ready to be replaced by String.Format.
+        /// </returns>
         private static string GetCommand(Command command)
         {
             var cmdString = "";
@@ -88,6 +125,11 @@ namespace StockFighter
             return cmdString;
         }
 
+        /// <summary>
+        /// Checks if the response was successful.
+        /// </summary>
+        /// <param name="response">The response received from the request.</param>
+        /// <returns>Returns true if the request was completed; else, false.</returns>
         private static bool IsSuccessful(IRestResponse response)
         {
             return response.ResponseStatus == ResponseStatus.Completed;
@@ -97,22 +139,59 @@ namespace StockFighter
         #endregion
     }
 
-    public class HeartbeatResponse
+    /// <summary>
+    /// Deserialized response from a Heartbeat command.
+    /// </summary>
+    public class HeartbeatResponse : APIResponse
     {
+        /// <summary>
+        /// Heartbeat was heard or not.
+        /// </summary>
         public bool ok { get; set; }
+        /// <summary>
+        /// Error text (if no error, then empty).
+        /// </summary>
         public string error { get; set; }
     }
 
-    public class VenueHeartbeatResponse
+    /// <summary>
+    /// Deserialized response from a CheckVenue command.
+    /// </summary>
+    public class VenueHeartbeatResponse : APIResponse
     {
+        /// <summary>
+        /// Venue heartbeat was heard or not.
+        /// </summary>
         public bool ok { get; set; }
+        /// <summary>
+        /// Venue that heartbeat was queried for.
+        /// </summary>
         public string venue { get; set; }
+    }
+
+    /// <summary>
+    /// Base class for all responses. Should be used for commonalities.
+    /// </summary>
+    private class APIResponse
+    {
+        /// <summary>
+        /// Error text. If no error, will be an empty string.
+        /// </summary>
         public string error { get; set; }
     }
 
+    /// <summary>
+    /// API Commands.
+    /// </summary>
     public enum Command
     {
+        /// <summary>
+        /// Checks if the server is up.
+        /// </summary>
         HeartBeat,
+        /// <summary>
+        /// Checks if a venue is up.
+        /// </summary>
         CheckVenue
     }
 
