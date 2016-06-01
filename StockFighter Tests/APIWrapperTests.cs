bcfb93adc87f6e7f4c307ed76ff8901fa76065e1
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-01 09:04:47 -0400

    Begin tracking APIWrapperTests.cs in commit

---
+++ StockFighter Tests/APIWrapperTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StockFighter_Tests
+{
+    [TestFixture]
+    public class APIWrapperTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
