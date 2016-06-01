87806ee3f4e1db5630f0834fd375d9367ef0112c
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2016-06-01 00:56:40 -0400

    Added unsaved changes
    Forgot to save the UnitTest changes before pushing

--- StockFighter Tests/UnitTest1.cs
+++ StockFighter Tests/UnitTest1.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace StockFighter_Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void TestMethod()
         {
         }
     }
