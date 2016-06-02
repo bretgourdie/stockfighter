947ae338457f278505ca2105f85f7578956f517d
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-02 13:40:51 -0400

    Added Quote class and command string

--- StockFighter/APIWrapper.cs
+++ StockFighter/APIWrapper.cs
@@ -146,7 +146,8 @@ namespace StockFighter
                 { typeof(Heartbeat), "heartbeat" },
                 { typeof(VenueHeartbeat), "venues/{0}/heartbeat" },
                 { typeof(VenueStocks), "venues/{0}/stocks" },
-                { typeof(Orderbook), "venues/{0}/stocks/{1}" }
+                { typeof(Orderbook), "venues/{0}/stocks/{1}" },
+                { typeof(Quote), "venues/{0}/stocks/{1}/quote" }
             };
 
             var dict = new Dictionary<Type, string>();
@@ -266,12 +267,60 @@ namespace StockFighter
         }
     }
 
+    /// <summary>
+    /// Deserialized representation of a quote request.
+    /// </summary>
     public class Quote : APIResponse
     {
-
+        /// <summary>
+        /// The stocks's symbol.
+        /// </summary>
+        public string symbol { get; set; }
+        /// <summary>
+        /// The venue of the stock.
+        /// </summary>
+        public string venue { get; set; }
+        /// <summary>
+        /// The best price currently bid for the stock.
+        /// </summary>
+        public int bid { get; set; }
+        /// <summary>
+        /// The best price currently offered for the stock.
+        /// </summary>
+        public int ask { get; set; }
+        /// <summary>
+        /// The aggregate size of all orders at the best bid.
+        /// </summary>
+        public int bidSize { get; set; }
+        /// <summary>
+        /// The aggregate size of all orders at the best ask.
+        /// </summary>
+        public int askSize { get; set; }
+        /// <summary>
+        /// The aggregate size of all bids.
+        /// </summary>
+        public int bidDepth { get; set; }
+        /// <summary>
+        /// The aggregate size of all asks.
+        /// </summary>
+        public int askDepth { get; set; }
+        /// <summary>
+        /// The price of the last trade.
+        /// </summary>
+        public int last { get; set; }
+        /// <summary>
+        /// The quantity of the last trade.
+        /// </summary>
+        public int lastSize { get; set; }
+        /// <summary>
+        /// The timestamp of the last trade.
+        /// </summary>
+        public DateTime lastTrade { get; set; }
+        /// <summary>
+        /// The timestamp the quote was last updated server-side.
+        /// </summary>
+        public DateTime quoteTime { get; set; }
     }
-
-
 
     /// <summary>
     /// Base class for all responses. Should be used for commonalities.
