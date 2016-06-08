098d6bd2df2727b2e363ea4427305afb85ede9af
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-08 13:15:31 -0400

    Added documentation for APIRequest

--- StockFighter/Requests/APIRequest.cs
+++ StockFighter/Requests/APIRequest.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace StockFighter.Requests
 {
+    /// <summary>
+    /// The base class for all request classes.
+    /// </summary>
     public class APIRequest
     {
     }
