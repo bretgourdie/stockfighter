6b04b83382d20fe610401da910888e3b0f2413de
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-09 13:06:05 -0400

    Added Gamemaster API

---
+++ StockFighter/GamemasterAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+
+namespace StockFighter
+{
+    /// <summary>
+    /// Wraps Gamemaster API functions.
+    /// </summary>
+    public class GamemasterAPI
+    {
+        private string _apiKey;
+
+        public string APIKey { get; }
+
+        public GamemasterAPI(string apiKey)
+        {
+
+        }
+
+        
+    }
+}
