c69f550c44b95dba5414057f72924c6a6a2290b9
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-14 13:00:34 -0400

    Added RestartedLevel class

---
+++ StockFighter/Responses/RestartedLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFighter.Responses
+{
+    /// <summary>
+    /// Deserialized response from a Restart Level command.
+    /// </summary>
+    class RestartedLevel : APIResponse
+    {
+    }
+}
