240a59a75fa1f3ddfb8371f2c0e456b1bea53391
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-14 13:17:19 -0400

    Added InstanceDetails class

---
+++ StockFighter/Responses/InstanceDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFighter.Responses
+{
+    public class InstanceDetails : APIResponse
+    {
+        public Details details { get; set; }
+        public bool done { get; set; }
+        public int id { get; set; }
+        public string state { get; set; }
+
+        public class Details
+        {
+            public int endOfTheWorldDay { get; set; }
+            public int tradingDay { get; set; }
+        }
+    }
+}
