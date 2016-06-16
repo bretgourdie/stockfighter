4bc78c7f7a19e7559a82f95968a5cf3ca64cf7ab
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-16 13:02:56 -0400

    Added Level01 solution

---
+++ StockFighter/Solutions/Level01_First_Steps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFighter.Solutions
+{
+    /// <summary>
+    /// The solution to the first level, First_Steps.
+    /// </summary>
+    public class Level01_First_Steps
+    {
+
+    }
+}
