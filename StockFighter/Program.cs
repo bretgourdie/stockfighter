8f1afb4994a050e2406ba293b0267fa87834b6fb
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-16 13:10:21 -0400

    Put solution in Solution class

--- StockFighter/Program.cs
+++ StockFighter/Program.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using StockFighter.API;
-using StockFighter.API.Requests;
-using StockFighter.Gamemaster;
+using System.Configuration;
+using StockFighter.Solutions;
 
 namespace StockFighter
 {
@@ -14,87 +13,10 @@ namespace StockFighter
     {
         static void Main(string[] args)
         {
-            var wrapper = new StockFighterAPI();
-            var gamemaster = new GamemasterAPI();
+            var apiKey = ConfigurationManager.AppSettings["apiKey"];
 
-            try
-            {
-                Console.Write("Starting level... ");
-                var levelInfo = gamemaster.StartLevel("first_steps");
-                Console.WriteLine(levelInfo.ok + "!");
-
-                var account = levelInfo.account;
-                var instanceId = levelInfo.instanceId;
-                var venues = levelInfo.venues;
-
-                Console.WriteLine(
-                    "instanceId: " + instanceId
-                    + "account: " + account);
-                
-
-                foreach (var venue in venues)
-                {
-                    var stocks = wrapper.GetStocks(venue);
-
-                    Console.WriteLine("Stocks for venue \"" + venue + "\":");
-
-                    foreach (var stock in stocks.symbols)
-                    {
-                        Console.WriteLine("\t" + stock.symbol + " (" + stock.name + ")");
-
-                        var quote = wrapper.GetQuote(venue, stock.symbol);
-
-                        Console.WriteLine("\tQuote:");
-                        Console.WriteLine("\t\tAsk: " + quote.ask);
-                        Console.WriteLine("\t\tBid: " + quote.bid);
-                        Console.WriteLine("\t\tTime: " + quote.quoteTime);
-                        Console.WriteLine("\t\tLast trade at: " + quote.lastTrade);
-                        Console.WriteLine("\n");
-
-                        var orderbook = wrapper.GetOrderbook(venue, stock.symbol);
-
-                        Console.WriteLine("\tBids: ");
-                        foreach (var bid in orderbook.bids)
-                        {
-                            Console.WriteLine("\t\t" + bid.qty + " @ $" + bid.price);
-                        }
-
-                        Console.WriteLine("\n\tAsks: ");
-                        foreach (var ask in orderbook.asks)
-                        {
-                            Console.WriteLine("\t\t" + ask.qty + " @ $" + ask.price);
-                        }
-
-                        // Attempt to buy 100 shares
-                        var orderRequest = new OrderRequest(
-                            account,
-                            venue,
-                            stock.symbol,
-                            0,
-                            100,
-                            OrderDirection.Buy,
-                            OrderType.Market);
-
-                        var orderResponse = wrapper.PostOrder(orderRequest);
-                        if (orderResponse != null && orderResponse.fills.Count > 0)
-                        {
-                            Console.WriteLine("\tFills:");
-
-                            foreach (var fill in orderResponse.fills)
-                            {
-                                Console.WriteLine("\t\t" + fill.qty + " @ $" + fill.price);
-                            }
-                        }
-                    }
-                }
-            }
-
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.ReadKey(true);
+            var level1 = new Level01_First_Steps(apiKey);
+            level1.Solve();
         }
     }
 }
--- StockFighter/Solutions/Level01_First_Steps.cs
+++ StockFighter/Solutions/Level01_First_Steps.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using StockFighter.API;
+using StockFighter.Gamemaster;
+
 namespace StockFighter.Solutions
 {
     /// <summary>
@@ -11,6 +14,97 @@ namespace StockFighter.Solutions
     /// </summary>
     public class Level01_First_Steps
     {
+        /// <summary>
+        /// The assigned APIKey for the level
+        /// </summary>
+        public string APIKey { get; private set; }
 
+        public Level01_First_Steps(string apiKey)
+        {
+
+        }
+
+        public bool Solve()
+        {
+            var wrapper = new StockFighterAPI();
+            var gamemaster = new GamemasterAPI();
+
+            try
+            {
+                Console.Write("Starting level... ");
+                var levelInfo = gamemaster.StartLevel("first_steps");
+                Console.WriteLine(levelInfo.ok + "!");
+
+                var account = levelInfo.account;
+                var instanceId = levelInfo.instanceId;
+                var venues = levelInfo.venues;
+
+                Console.WriteLine(
+                    "instanceId: " + instanceId
+                    + "account: " + account);
+                
+
+                foreach (var venue in venues)
+                {
+                    var stocks = wrapper.GetStocks(venue);
+
+                    Console.WriteLine("Stocks for venue \"" + venue + "\":");
+
+                    foreach (var stock in stocks.symbols)
+                    {
+                        Console.WriteLine("\t" + stock.symbol + " (" + stock.name + ")");
+
+                        var quote = wrapper.GetQuote(venue, stock.symbol);
+
+                        Console.WriteLine("\tQuote:");
+                        Console.WriteLine("\t\tAsk: " + quote.ask);
+                        Console.WriteLine("\t\tBid: " + quote.bid);
+                        Console.WriteLine("\t\tTime: " + quote.quoteTime);
+                        Console.WriteLine("\t\tLast trade at: " + quote.lastTrade);
+                        Console.WriteLine("\n");
+
+                        var orderbook = wrapper.GetOrderbook(venue, stock.symbol);
+
+                        Console.WriteLine("\tBids: ");
+                        foreach (var bid in orderbook.bids)
+                        {
+                            Console.WriteLine("\t\t" + bid.qty + " @ $" + bid.price);
+                        }
+
+                        Console.WriteLine("\n\tAsks: ");
+                        foreach (var ask in orderbook.asks)
+                        {
+                            Console.WriteLine("\t\t" + ask.qty + " @ $" + ask.price);
+                        }
+
+                        // Attempt to buy 100 shares
+                        var orderRequest = new API.Requests.OrderRequest(
+                            account,
+                            venue,
+                            stock.symbol,
+                            0,
+                            100,
+                            OrderDirection.Buy,
+                            OrderType.Market);
+
+                        var orderResponse = wrapper.PostOrder(orderRequest);
+                        if (orderResponse != null && orderResponse.fills.Count > 0)
+                        {
+                            Console.WriteLine("\tFills:");
+
+                            foreach (var fill in orderResponse.fills)
+                            {
+                                Console.WriteLine("\t\t" + fill.qty + " @ $" + fill.price);
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
