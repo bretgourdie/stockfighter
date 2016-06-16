b752e054950ea1109d2377dede77cc8b93607d94
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-16 13:13:10 -0400

    Added ISolvable for solutions

---
+++ StockFighter/Solutions/ISolvable.cs
@@ -0,0 +1,15 @@
+﻿namespace StockFighter.Solutions
+{
+    /// <summary>
+    /// A class implementing this interface must (attempt to) solve a level and
+    /// report its solvedness.
+    /// </summary>
+    public interface ISolvable
+    {
+        /// <summary>
+        /// A method to solve the level.
+        /// </summary>
+        /// <returns>Returns if the level was solved or not.</returns>
+        bool Solve();
+    }
+}
