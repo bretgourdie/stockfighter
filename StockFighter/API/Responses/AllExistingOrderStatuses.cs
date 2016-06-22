6f5f577361859d479a0c205167eaf6b1bdaef615
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-22 15:52:29 -0400

    Added AllExistingOrderStatuses.cs

---
+++ StockFighter/API/Responses/AllExistingOrderStatuses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFighter.API.Responses
+{
+    /// <summary>
+    /// Client-facing response from querying all existing orders.
+    /// </summary>
+    public class AllExistingOrderStatuses : Common.APIResponse
+    {
+        /// <summary>
+        /// The venue of the orders.
+        /// </summary>
+        public string venue { get; set; }
+        /// <summary>
+        /// The status of each order.
+        /// </summary>
+        public List<ExistingOrderStatus> orders { get; set; }
+    }
+}
