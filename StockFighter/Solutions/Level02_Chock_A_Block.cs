8a8961ec804737421ecca954a421607c5d9802d5
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-23 15:04:06 -0400

    Added Level 2 Solution

---
+++ StockFighter/Solutions/Level02_Chock_A_Block.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFighter.Solutions
+{
+    /// <summary>
+    /// The solution to the second level, Chock_A_Block.
+    /// </summary>
+    public class Level02_Chock_A_Block : ISolvable<Level02_Chock_A_Block>
+    {
+
+    }
+}
