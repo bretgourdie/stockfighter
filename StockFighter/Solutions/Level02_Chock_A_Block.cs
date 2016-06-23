3567926e7f8c25465df7c7707cba83a16d0351ba
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-23 15:24:58 -0400

    Added part of Solve()
    Very basic outline of variables involved

--- StockFighter/Solutions/Level02_Chock_A_Block.cs
+++ StockFighter/Solutions/Level02_Chock_A_Block.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using StockFighter.API;
+using StockFighter.Gamemaster;
+
 namespace StockFighter.Solutions
 {
     /// <summary>
@@ -11,6 +14,16 @@ namespace StockFighter.Solutions
     /// </summary>
     public class Level02_Chock_A_Block : ISolvable<Level02_Chock_A_Block>
     {
+        protected string apiKey { get; set; }
+
+        /// <summary>
+        /// Creates an initialized Chock_A_Block solution using the specified API key.
+        /// </summary>
+        /// <param name="apiKey">The API key to use for th eleve.</param>
+        public Level02_Chock_A_Block(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
         /// <summary>
         /// Return the name of the level.
         /// </summary>
@@ -34,7 +47,13 @@ namespace StockFighter.Solutions
         /// <returns>Returns if the level was solved or not.</returns>
         public bool Solve()
         {
+            var wrapper = new StockFighterAPI(this.apiKey);
+            var gamemaster = new GamemasterAPI(this.apiKey);
 
+            var sharesToSell = 100000;
+            var solved = false;
+
+            return solved;
         }
     }
 }
