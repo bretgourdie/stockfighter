dbdfac33d67cc3800e105a2f0a6b6d2d7d31c33d
bretgourdie <adjectivenounisadjective@gmail.com>
2016-06-29 12:43:22 -0400

    Added Level 3 solution class

---
+++ StockFighter/Solutions/Level03_Sell_Side.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFighter.Solutions
+{
+    public class Level03_Sell_Side : ISolvable
+    {
+        public bool Solve()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LevelName
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
